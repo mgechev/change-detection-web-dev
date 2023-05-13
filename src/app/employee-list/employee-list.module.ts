9c33caeef0c4d3403a940837d54bb17a5b277310
phalgunv <phalgun251@gmail.com>
2023-05-13 17:44:33 +0530

    @angular/material migration - migration-v9
    Updates Angular Material to v9

--- src/app/employee-list/employee-list.module.ts
+++ src/app/employee-list/employee-list.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCommonModule } from '@angular/material';
+import { MatCommonModule } from '@angular/material/core';
 import { MatChipsModule, MatChip } from '@angular/material/chips';
 import { MatListModule } from '@angular/material/list';
-import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { EmployeeListComponent } from './employee-list.component';
 
