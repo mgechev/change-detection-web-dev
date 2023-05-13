afe21a3c59f218ac5a12288f8a39f9d39aa240d3
phalgunv <phalgun251@gmail.com>
2023-05-13 23:18:41 +0530

    @angular/material migration - migration-v15
    Updates the Angular Material to v15

--- src/app/employee-list/employee-list.module.ts
+++ src/app/employee-list/employee-list.module.ts
@@ -3,10 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCommonModule } from '@angular/material/core';
-import { MatChipsModule, MatChip } from '@angular/material/chips';
-import { MatListModule } from '@angular/material/list';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { MatLegacyChipsModule as MatChipsModule, MatLegacyChip as MatChip } from '@angular/material/legacy-chips';
+import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
+import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
+import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
 
 import { EmployeeListComponent } from './employee-list.component';
 
