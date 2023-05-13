b177d08ecb2e41ee77866b7685dd4566d62bd07b
phalgunv <phalgun251@gmail.com>
2023-05-14 02:18:22 +0530

    @angular/core:standalone - Remove unnecessary NgModule classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-import { EmployeeListModule } from './employee-list/employee-list.module';
+
 import { ListGenerator } from './tree-generator.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [BrowserModule, EmployeeListModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [ListGenerator],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
