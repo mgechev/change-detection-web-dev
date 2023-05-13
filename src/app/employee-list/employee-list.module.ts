ddc6b2484fa176f4934884303afcdeae10438284
phalgunv <phalgun251@gmail.com>
2023-05-14 01:47:05 +0530

    Fix deprecated angular material imports

--- src/app/employee-list/employee-list.module.ts
+++ src/app/employee-list/employee-list.module.ts
@@ -1,13 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCommonModule } from '@angular/material/core';
-import { MatLegacyChipsModule as MatChipsModule, MatLegacyChip as MatChip } from '@angular/material/legacy-chips';
-import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
-import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
-import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
-
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatChipsModule } from '@angular/material/chips';
 import { EmployeeListComponent } from './employee-list.component';
 
 @NgModule({
@@ -15,18 +12,13 @@ import { EmployeeListComponent } from './employee-list.component';
     CommonModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatListModule,
-    MatFormFieldModule,
     MatInputModule,
-    MatCommonModule,
-    MatChipsModule
+    MatListModule,
+    MatChipsModule,
   ],
   declarations: [EmployeeListComponent],
   exports: [
-    EmployeeListComponent,
-    MatInputModule,
-    MatCommonModule,
-    MatChipsModule
+    EmployeeListComponent
   ]
 })
 export class EmployeeListModule {}
